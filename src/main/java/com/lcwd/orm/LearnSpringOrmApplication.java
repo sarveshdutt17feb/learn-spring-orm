4be359ec26ad2142bd60c60bdfb69291d3fd37a2
Sarvesh Dutt Richhariya <sarveshdutt40@gmail.com>
2024-04-11 17:09:36 +0530

    creating new project with JPA

---
+++ src/main/java/com/lcwd/orm/LearnSpringOrmApplication.java
@@ -0,0 +1,13 @@
+package com.lcwd.orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcwd/orm/LearnSpringOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
