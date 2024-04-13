f22c93f8c847d486a6059639d42acc200e134d6f
Sarvesh Dutt Richhariya <sarveshdutt40@gmail.com>
2024-04-13 14:05:13 +0530

    Custom Query Methods in Detail NativeQuery

--- src/main/java/com/lcwd/orm/repositories/ProductRepo.java
+++ src/main/java/com/lcwd/orm/repositories/ProductRepo.java
@@ -1,7 +1,12 @@
 package com.lcwd.orm.repositories;
 
 import com.lcwd.orm.entities.Product;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import  java.util.*;
 public interface ProductRepo extends JpaRepository<Product,String> {
 
@@ -16,7 +21,7 @@ public interface ProductRepo extends JpaRepository<Product,String> {
 
     List<Product> findByProductNameIsNull();
 
-    List<Product> findByActiveTrue();
+    List<Product> findByActiveTrue(Pageable pageable);
     List<Product> findByActiveFalse();
 
     List<Product> findByProductNameStartingWith(String prefix);
@@ -43,4 +48,24 @@ public interface ProductRepo extends JpaRepository<Product,String> {
 
 
     //2.queryMethod
+    //select all products with query
+    //JPQL--> iske liye entity name dekhna padta h like Product
+    //JPQL specific query db independent
+    @Query(value = "select p from Product p")
+    List<Product> getAllProductWhileLearningJPA();
+    @Query("select p from Product p wherep.productName='Samsung'")
+    List<Product> getAllActiveProducts();
+    //dynamic param with pos
+    @Query("select p from Product where p.pId=?1 AND p.productName=?2")
+    Product getSingleProductByIdAndName1(String pid,String productName);
+    //named parameter
+    @Query("select p from Product where p.pId=:productId AND p.productName=:productName")
+    Product getSingleProductByIdAndName2(@Param("productId") String pid,@Param("productName") String productName);
+    //if you write update query then use @Modify annotation
+
+    //native query
+    //native query ke liye table dekhni padti db mn--> db specific query
+    @Query("select * from jpa_product where product_name=:productName ")
+    Product getProductByIdAndNameNative(@Param("productName") String productName);
+
 }
